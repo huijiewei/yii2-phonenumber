862d78c216989f02df4fcde446ffa249dcde3f9d
Huijie Wei <huijiewei@outlook.com>
2019-05-30 11:33:06 +0800

    优化输入

---
+++ src/IntlTelInputJsWidget.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Huijiewei
+ * Date: 2014/11/27
+ * Time: 15:53
+ */
+
+namespace huijiewei\phonenumber;
+
+use yii\base\Widget;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
+use yii\web\AssetBundle;
+use yii\web\View;
+
+class IntlTelInputJsWidget extends Widget
+{
+    public $querySelector = [];
+
+    public $onLoadComponent = false;
+
+    public $clientOptions = [];
+
+    /* @var $_assetBundle AssetBundle */
+    private $_assetBundle;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->clientOptions = ArrayHelper::merge([
+            'initialCountry' => 'cn',
+            'placeholderNumberType' => 'MOBILE',
+            'separateDialCode' => false,
+            'autoPlaceholder' => 'aggressive',
+            'formatOnDisplay' => false,
+            'preferredCountries' => ['us', 'gb', 'cn'],
+        ], $this->clientOptions);
+
+        $this->registerAssetBundle();
+
+        $this->registerScript();
+    }
+
+    public function registerAssetBundle()
+    {
+        $this->_assetBundle = IntlTelInputAsset::register($this->getView());
+    }
+
+    public function registerScript()
+    {
+        $clientOptions = Json::encode($this->clientOptions);
+
+        $js = <<<EOD
+var telInput = document.querySelector('{$this->querySelector}');
+if(telInput) {
+    var intlTelInput = window.intlTelInput(telInput, {$clientOptions});
+   
+    $(telInput).on("keyup change", resetIntlTelInput);
+
+    function resetIntlTelInput() {
+      if (typeof intlTelInputUtils !== 'undefined') {
+          var currentText = intlTelInput.getNumber(intlTelInputUtils.numberFormat.E164);
+          console.log(currentText);
+          if (typeof currentText === 'string') {
+              $(this).val(currentText);
+          }
+      }
+    }
+}
+EOD;
+
+        if ($this->onLoadComponent) {
+            $this->getView()->registerJs('$(document.body).on(\'loadComponent\', function (e) {' . $js . '});', View::POS_END);
+        } else {
+            $this->getView()->registerJs($js);
+        }
+
+    }
+
+    public function getAssetBundle()
+    {
+        if (!($this->_assetBundle instanceof AssetBundle)) {
+            $this->registerAssetBundle();
+        }
+
+        return $this->_assetBundle;
+    }
+}
--- src/IntlTelInputWidget.php
+++ src/IntlTelInputWidget.php
@@ -10,8 +10,6 @@ namespace huijiewei\phonenumber;
 
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
-use yii\helpers\Json;
-use yii\web\AssetBundle;
 use yii\widgets\InputWidget;
 
 class IntlTelInputWidget extends InputWidget
@@ -20,9 +18,6 @@ class IntlTelInputWidget extends InputWidget
 
     public $clientOptions = [];
 
-    /* @var $_assetBundle AssetBundle */
-    private $_assetBundle;
-
     public function init()
     {
         parent::init();
@@ -30,49 +25,16 @@ class IntlTelInputWidget extends InputWidget
         $this->options = ArrayHelper::merge([
             'class' => 'form-control',
         ], $this->options);
-
-        $this->clientOptions = ArrayHelper::merge([
-            'initialCountry' => 'cn',
-            'placeholderNumberType' => 'MOBILE',
-            'preferredCountries' => ['us', 'gb', 'cn'],
-        ], $this->clientOptions);
-
-        $this->registerAssetBundle();
-
-        $this->registerScript();
-    }
-
-    public function registerAssetBundle()
-    {
-        $this->_assetBundle = IntlTelInputAsset::register($this->getView());
-    }
-
-    public function registerScript()
-    {
-        $clientOptions = Json::encode($this->clientOptions);
-
-        $js = <<<EOD
-$('#{$this->options['id']}').intlTelInput({$clientOptions});
-EOD;
-
-        $this->getView()->registerJs($js);
     }
 
     public function run()
     {
+        echo IntlTelInputJsWidget::widget(['querySelector' => '#' . $this->options['id'], 'clientOptions' => $this->clientOptions]);
+
         if ($this->hasModel()) {
             return Html::activeTextInput($this->model, $this->attribute, $this->options);
         } else {
             return Html::textInput($this->name, $this->value, $this->options);
         }
-    }
-
-    public function getAssetBundle()
-    {
-        if (!($this->_assetBundle instanceof AssetBundle)) {
-            $this->registerAssetBundle();
-        }
-
-        return $this->_assetBundle;
     }
 }
